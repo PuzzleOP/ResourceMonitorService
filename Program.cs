8a9d744e6fb1b84bcd1c3f679214e0cd8ee0c395
PuzzleOP <obsidiankinez@gmail.com>
2021-06-09 10:37:40 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace ResourceMonitorService
+{
+    class Program
+    {
+        public static int memorySize;
+        public static object tvms;
+
+        static void Main(string[] args)
+        {
+            ObjectQuery wql = new ObjectQuery("SELECT * FROM Win32_OperatingSystem");
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(wql);
+            ManagementObjectCollection results = searcher.Get();
+
+            foreach (ManagementObject result in results)
+            {
+                tvms = result["TotalVisibleMemorySize"];
+                memorySize = Convert.ToInt32(tvms) / 1024;
+
+            }
+
+            var runValue = HostFactory.Run(x => 
+            {
+                x.Service<ResourceMonitor>(y =>
+                {
+                    y.ConstructUsing(rm => new ResourceMonitor());
+                    y.WhenStarted(rm => rm.Start());
+                    y.WhenStopped(rm => rm.Stop());
+                });
+                
+                x.RunAsLocalSystem();
+
+                x.SetServiceName("ResourceMonitorService");
+                x.SetDisplayName("Resource Monitor Service Demo");
+                x.SetDescription("A Resource Monitor Service done as a demo");
+
+            });
+
+            int runValueInteger = (int) Convert.ChangeType(runValue, runValue.GetTypeCode());
+            Environment.ExitCode = runValueInteger;
+
+        }
+    }
+}
---
+++ ResourceMonitor.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ResourceMonitorService
+{
+    class ResourceMonitor
+    {
+        private readonly Timer t;
+
+        static int scanFrequency = 2000;
+        string filePath = @"D:\Ivan\Repos\ResourceMonitorLog.txt";
+
+        PerformanceCounter pCPUCounter = new PerformanceCounter("Processor Information", "% Processor Time", "_Total");
+        PerformanceCounter pMemoryAvailable = new PerformanceCounter("Memory", "Available MBytes");
+        PerformanceCounter pSystemUpTime = new PerformanceCounter("System", "System Up Time");
+        PerformanceCounter pMemoryBytesCached = new PerformanceCounter("Memory","Cache Bytes");
+
+
+        public ResourceMonitor()
+        {
+            t = new Timer(scanFrequency) { AutoReset = true };
+            t.Elapsed += timerElapsed;
+            
+        }
+
+        private void timerElapsed(object sender, ElapsedEventArgs e)
+        {
+            string[] lines = new string[]
+            {
+                DateTime.Now.ToString() + " ==>  \nCPU: " +(int)pCPUCounter.NextValue() + 
+                "%\nMemory: " + (int)pMemoryAvailable.NextValue() +
+                " MB /" + Program.memorySize + " MB\nMemory Cached Bytes: " +
+                ((int)pMemoryBytesCached.NextValue() / 1024) / 1024 +
+                " MB\nSystem Up Time: " + (int)pSystemUpTime.NextValue() / 60 +
+                " minutes " + (int)pSystemUpTime.NextValue() % 60 +
+                " seconds\n------------------------"
+            };
+
+            File.AppendAllLines(filePath, lines);
+        }
+
+        public void Start()
+        {
+            t.Start();
+        }
+
+        public void Stop()
+        {
+            t.Stop();
+        }
+
+    }
+}
